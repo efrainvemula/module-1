f166bd26cff75c4a247a6a197e1022801e04cbbb
efrainvemula <priya.surapaneni08@gmail.com>
2026-09-16 19:52:02 +1000

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,20 @@
+Age= 10
+Name= "Efrain"
+Score= 90.5
+Eligibility= False
+print (type (Age))
+print (type (Name))
+print (type (Score))
+print (type (Eligibility))
+
+# Float casting
+Age1= float(Age)
+# Int casting
+Score1= int(Score)
+print (Age1, Score1)
+
+word= "Mango"
+print (word[2:4])
+f1= "apple"
+f2= "banana"
+print (f1 + f2)
